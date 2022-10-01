fc40cbe5702550b3b924e1a914b67e6b0fc47ec5
unknown <gustavommessa@gmail.com>
2022-10-01 13:42:20 -0300

    alteração Heaader

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header(){
    
@@ -7,9 +8,9 @@ function Header(){
         <nav>
             <ul>
                 <img alt="novob" src="../assets/img/MIDALL NOVOb.png" class="imagem" />
-                <li><a href="/AlterEmail">Clipping de Email</a></li>
-                <li><a href="/AlterAssoc">Gerenciar Associados</a></li>
-                <li><a href="CadAssoc">Cadastrar Associados</a></li>
+                <li><Link to="/AlterEmail">Clipping de Email</Link></li>
+                <li><Link to="/AlterAssoc">Gerenciar Associados</Link></li>
+                <li><Link to="/CadAssoc">Cadastrar Associados</Link></li>
                 <img alt="lnovob" src="../assets/img/MIDALL NOVOb.png" class="imagem1" />
             </ul>
         </nav>
